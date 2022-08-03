4d905c6ef613f35114de0bfb09f663186bf3e210
Felipe Bastos Nunes <felipe.bastosn@gmail.com>
2022-08-03 17:16:35 +0000

    fix for mysql

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,22 +84,15 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        'NAME': f"{os.getenv('user')}${os.getenv('database')}",
+        'USER': os.getenv('user'),
+        'PASSWORD': os.getenv('password'),
+        'HOST': os.getenv('host'),
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.mysql",
-            "OPTIONS": {
-                "read_default_file": os.getenv("MYSQLCONF", None),
-            },
-        }
-    }
+}
 
 
 # Password validation
