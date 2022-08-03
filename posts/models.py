805bf2e32b30079d60bfedc90c2b1c440692593c
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2022-08-03 14:38:40 -0300

    Bugfix

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
-    titulo = models.TextField(max_length=144, unique=True)
-    texto = models.TextField(max_length=5000)
+    titulo = models.CharField(max_length=144, unique=True)
+    texto = models.CharField(max_length=5000)
     data = models.DateTimeField(auto_now_add=True)
