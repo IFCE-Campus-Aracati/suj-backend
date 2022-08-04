51df47c3d9cd651b90f20cb2fafb19af2a1925a6
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2022-08-04 09:53:08 -0300

    Posts terão tags.

--- posts/models.py
+++ posts/models.py
@@ -2,8 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Tag(models.Model):
+    label = models.CharField(max_length=10, unique=True)
+
+    class Meta:
+        ordering = ["label"]
+
+    def __str__(self) -> str:
+        return self.label
+
+
 class Post(models.Model):
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=144, unique=True)
     texto = models.CharField(max_length=5000)
     data = models.DateTimeField(auto_now_add=True)
+
+    tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        ordering = ["data"]
+
+    def __str__(self) -> str:
+        return self.titulo
